92c373c0c32a685d724befd5c967a03e6098253b
Deepesh-aw <deepesh@ascenwork.com>
2020-05-26 19:06:21 +0530

    Changes for project creation action

---
+++ DeepeshWeb/Controllers/Timesheet/ProjectCreationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeepeshWeb.Controllers.TimeSheet
+{
+    public class ProjectCreationController : Controller
+    {
+        // GET: ProjectCreation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DeepeshWeb/Models/Timesheet/TIM_ProjectCreationModel.cs
+++ DeepeshWeb/Models/Timesheet/TIM_ProjectCreationModel.cs
@@ -9,6 +9,17 @@ namespace DeepeshWeb.Models.Timesheet
     {
         public int Id { get; set; }
         public string ProjectName { get; set; }
-        public string ProjectName { get; set; }
+        public int ClientName { get; set; }
+        public string ClientProjectManager { get; set; }
+        public string Description { get; set; }
+        public DateTime EndDate { get; set; }
+        public string InternalStatus { get; set; }
+        public int Members { get; set; }
+        public string MembersText { get; set; }
+        public int NoOfDays { get; set; }
+        public int ProjectManager { get; set; }
+        public DateTime StartDate { get; set; }
+        public int ProjectType { get; set; }
+        public int Status { get; set; }
     }
 }
