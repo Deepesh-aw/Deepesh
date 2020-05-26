593187002bec22c762a0a55721105fd946dcf5b4
Deepesh-aw <deepesh@ascenwork.com>
2020-05-26 12:36:01 +0530

    Development Code Updated

---
+++ DeepeshWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeepeshWeb.MvcApplication" Language="C#" %>
---
+++ DeepeshWeb/Models/Timesheet/TIM_ProjectCreationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeepeshWeb.Models.Timesheet
+{
+    public class TIM_ProjectCreationModel
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectName { get; set; }
+    }
+}
